15a4d2e9fd46ddf88e7b1e0db0011d66d4ffad30
Tapiwanashe Shoshore <tapiwanashe.shoshore@wonderlabz.com>
2022-02-22 21:18:41 +0200

    first commit

---
+++ src/main/java/com/shoshore/voters/VotersApplication.java
@@ -0,0 +1,13 @@
+package com.shoshore.voters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotersApplication.class, args);
+	}
+
+}
